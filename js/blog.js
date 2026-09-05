57863fc7af290a63f7a1f8cd5c3474032ec55d92
IOR14 <israel.or.efi@gmail.com>
2026-09-04 23:26:28 -0400

    Restyle fatigue and diabetes infographics to white brand template

--- js/blog.js
+++ js/blog.js
@@ -31,7 +31,7 @@
     var date = post.published_at ? formatDate(post.published_at) : '';
     var image = post.image ? escapeHtml(post.image) : '';
     var hero = image
-      ? '<img src="' + image + '?v=10" alt="' + escapeHtml(post.title) + '" class="w-full h-full object-cover">'
+      ? '<img src="' + image + '?v=11" alt="' + escapeHtml(post.title) + '" class="w-full h-full object-cover">'
       : '<div class="w-full h-full bg-gradient-to-br from-metabolic-green/20 to-metabolic-cyan/20 flex items-center justify-center">' +
           '<svg class="w-16 h-16 text-' + color + '" fill="none" stroke="currentColor" viewBox="0 0 24 24">' +
             '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>' +
